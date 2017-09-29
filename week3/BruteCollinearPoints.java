b483a548903a5f1c7f6950acebca628451219d3f
faris <zhufaris@gmail.com>
2017-09-29 10:18:25 +0800

    test

--- week3/BruteCollinearPoints.java
+++ week3/BruteCollinearPoints.java
@@ -2,13 +2,6 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 public class BruteCollinearPoints {
-<<<<<<< HEAD
-    private final Point[] points;
-    private int length;
-
-    public BruteCollinearPoints(Point[] points) {
-        if (points == null) throw new IllegalArgumentException();
-=======
     private final ArrayList<LineSegment> segmentlist = new ArrayList<>();
     private final Point[] points;
 
@@ -19,7 +12,6 @@ public class BruteCollinearPoints {
         if (points == null) {
             throw new IllegalArgumentException();
         }
->>>>>>> 0c65e3784be9e3d7806085190c196e30b395e305
         this.points = Arrays.copyOf(points, points.length);
         for (int i = 0; i < this.points.length; i++) {
             if (this.points[i] == null) throw new IllegalArgumentException();
@@ -28,27 +20,6 @@ public class BruteCollinearPoints {
         for (int i = 0; i < this.points.length - 1; i++) {
             if (this.points[i].compareTo(this.points[i+1]) == 0) throw new IllegalArgumentException();
         }
-<<<<<<< HEAD
-        this.length = 0;
-    }
-
-    public int numberOfSegments() {
-        return length;
-    }
-
-    public LineSegment[] segments() {
-        ArrayList<LineSegment> segmentlist = new ArrayList<>();
-        for (int p = 0; p < points.length - 3; p++)
-        for (int q = p + 1; q < points.length - 2; q++)
-        for (int r = q + 1; r < points.length - 1; r++)
-        for (int s = r + 1; s < points.length; s++){
-            if ((points[p].slopeTo(points[q]) == points[q].slopeTo(points[r]) 
-            && points[q].slopeTo(points[r]) == points[r].slopeTo(points[s])))
-            segmentlist.add(new LineSegment(points[p], points[s]));
-        }
-        length = segmentlist.size();
-        LineSegment[] seg = new LineSegment[length];
-=======
         findSegments();
     }
 
@@ -70,7 +41,6 @@ public class BruteCollinearPoints {
 
     public LineSegment[] segments() {
         LineSegment[] seg = new LineSegment[segmentlist.size()];
->>>>>>> 0c65e3784be9e3d7806085190c196e30b395e305
         return segmentlist.toArray(seg);
     }
 }
--- week3/Point.java
+++ week3/Point.java
@@ -13,11 +13,7 @@ public class Point implements Comparable<Point> {
         this.y = y;
     }
     
-<<<<<<< HEAD
-    public void draw(){
-=======
     public void draw() {
->>>>>>> 0c65e3784be9e3d7806085190c196e30b395e305
         StdDraw.point(x, y);
     }
 
@@ -33,20 +29,7 @@ public class Point implements Comparable<Point> {
         validate(that);
         if (this.x == that.x && this.y == that.y) return Double.NEGATIVE_INFINITY;
         if (this.x == that.x) return Double.POSITIVE_INFINITY;
-<<<<<<< HEAD
-        return 1.0 *(that.y - this.y)/(that.x -this.x);
-=======
-        return absolute(1.0 * (that.y - this.y)/(that.x -this.x));
     }
-
-    private double absolute(double x) {
-        if ( 1/ x > 0) return x;
-        else return -1 * x;
->>>>>>> 0c65e3784be9e3d7806085190c196e30b395e305
-    }
-
-    @Override
-    public int compareTo(Point that) {
         validate(that);
         if (this.y < that.y) return -1;
         else if (this.y > that.y) return 1;
@@ -62,11 +45,7 @@ public class Point implements Comparable<Point> {
             @Override
             public int compare(Point a, Point b) {
                 if (slopeTo(a) < slopeTo(b)) return -1;
-<<<<<<< HEAD
-                else if(slopeTo(a) == slopeTo(b)) return 0;
-=======
                 else if (slopeTo(a) == slopeTo(b)) return 0;
->>>>>>> 0c65e3784be9e3d7806085190c196e30b395e305
                 else return 1; 
             }
         };
@@ -75,13 +54,10 @@ public class Point implements Comparable<Point> {
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
-<<<<<<< HEAD
-=======
 
     public static void main(String[] args) {
         Point a = new Point(391, 263);
         Point b = new Point(130, 263);
         StdOut.println(b.slopeTo(a));
     }
->>>>>>> 0c65e3784be9e3d7806085190c196e30b395e305
 }
